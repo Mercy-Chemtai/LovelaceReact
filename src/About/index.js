3c7d78924df7eb96e12c5b39f61050b67c4a2cef
Mercy-Chemtai <chemtailodite@gmail.com>
2024-06-20 10:52:15 +0300

    add Users and Login files

---
+++ src/About/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+const About = ()=>{
+    return (
+        <div>
+            <p className="about">I am learning ReactJS which is a Javascript Front-end library</p>
+        </div>
+    )
+}
+export default About
---
+++ src/Login/index.js
@@ -0,0 +1,28 @@
+import './index.css'
+import { useState } from "react";
+import { login } from './utils';
+const Login = ()=>{
+    const [username,setUserName] = useState('');
+    const [password,setPassword] = useState('');
+    console.log({username})
+
+const handleLogin = async(e) =>{
+    e.preventDefault();
+    console.log('are we here');
+    const result= await login ({username,password});
+    console.log({result}) 
+};
+
+    return (
+        <form onSubmit={handleLogin}>
+            <h2>Login</h2>
+        <input placeholder="Enter username" type="text" onChange={(e)=>setUserName(e.target.value)}/>
+        <br/>
+        <input placeholder="Enter password" type="password" onChange={(e)=>setPassword(e.target.value)}/>
+        <br/>
+        <button type="submit">Login</button>
+        
+        </form>
+    )
+}
+export default Login
